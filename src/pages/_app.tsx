07efc0771cd1c5e6f737199048362f9bd47fa519
Nilo Ferreira <nilo_ferreira@hotmail.com>
2023-02-10 11:20:23 -0300

    refactor: title

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import type { AppProps } from 'next/app';
 import { AuthProvider } from '@/application/hooks/use-auth';
 import '@/styles/globals.css';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import Head from 'next/head';
 
 const queryClient = new QueryClient();
 
@@ -9,6 +10,9 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <AuthProvider>
       <QueryClientProvider client={queryClient}>
+        <Head>
+          <title>Instituto Vitaliza</title>
+        </Head>
         <Component {...pageProps} />
       </QueryClientProvider>
     </AuthProvider>
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,7 +5,6 @@ export default function Document() {
   return (
     <Html lang='en'>
       <Head>
-        <title>Instituto Vitaliza</title>
         <link
           rel='icon'
           type='image/svg+xml'
